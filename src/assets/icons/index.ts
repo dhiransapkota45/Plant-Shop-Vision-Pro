d004fe5bb135b6c90477dcfdd0d4a8b0ac60c2e6
dhiransapkota45 <dhiransapkota55@gmail.com>
2024-03-26 21:52:48 +0545

    design: layout setup

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import HomeIcon from "./home.svg";
+import FlowerIcon from "./flower.svg";
+import CartIcon from "./cart.svg";
+import SettingIcon from "./settings.svg"
+
+export { HomeIcon, CartIcon, FlowerIcon, SettingIcon };
---
+++ src/data/routes.tsx
@@ -0,0 +1,24 @@
+import { CartIcon, FlowerIcon, HomeIcon, SettingIcon } from "@/assets/icons";
+
+export const routes = [
+    {
+        title:"home",
+        link:"/",
+        icon: HomeIcon
+    },
+    {
+        title:"Products",
+        link:"/products",
+        icon : FlowerIcon
+    },
+    {
+        title:"Cart",
+        link:"/cart",
+        icon:CartIcon
+    },
+    {
+        title:"Settings",
+        link:"/settings",
+        icon:SettingIcon
+    }
+]
