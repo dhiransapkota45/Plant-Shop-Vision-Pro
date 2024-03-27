b274d341d113189471aa8bcbd3a338bd6bc29006
dhiransapkota45 <dhiransapkota55@gmail.com>
2024-03-27 23:39:53 +0545

    fix: design fixes at sidenavigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { routes } from "@/data/routes";
-import Image from "next/image";
-import Link from "next/link";
 import ToasterProvider from "@/providers/ToasterProvider";
+import SideNavigation from "@/components/layout/SideNavigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,33 +12,20 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
+  context,
   children,
 }: Readonly<{
+  context: any;
   children: React.ReactNode;
 }>) {
+  console.log(context, "dhiran")
   return (
     <html lang="en">
       <body className={inter.className}>
         <ToasterProvider>
           <div className="bg-cover bg-no-repeat bg-[url('../assets/images/background.jpg')]">
             <div className="  container flex gap-10  justify-center items-center h-screen w-full ">
-              <div className="flex gap-4 shrink-0 backdrop-blur-lg bg-white bg-opacity-40 px-3 py-8 rounded-full flex-col ">
-                {routes?.map((route) => (
-                  <Link
-                    href={route.link}
-                    className=" cursor-pointer animation bg-white bg-opacity-0 hover:bg-opacity-30 p-4 rounded-full "
-                    key={route.link}
-                  >
-                    <Image
-                      className=" "
-                      width={25}
-                      height={25}
-                      src={route.icon}
-                      alt={route.title}
-                    />
-                  </Link>
-                ))}
-              </div>
+              <SideNavigation />
               <div className="  text-white max-h-[700px] h-full w-[700px]  shrink-0 flex-1    bg-white  rounded-2xl bg-opacity-40  backdrop-blur-lg">
                 {children}
               </div>
---
+++ src/components/layout/SideNavigation.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { routes } from "@/data/routes";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+
+type Props = {};
+
+const SideNavigation = (props: Props) => {
+    const pathname = usePathname();
+    return (
+    <div className="flex gap-4 shrink-0 backdrop-blur-lg bg-white bg-opacity-40 px-3 py-8 rounded-full flex-col ">
+      {routes?.map((route) => (
+        <Link
+          href={route.link}
+          className={`cursor-pointer animation bg-white bg-opacity-0 hover:bg-opacity-30 p-4 rounded-full ${pathname===route.link ? 'bg-opacity-30' : ''}`}
+          key={route.link}
+        >
+          <Image
+            className=" "
+            width={25}
+            height={25}
+            src={route.icon}
+            alt={route.title}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default SideNavigation;
